a48e989f38f7476337762725bff9769da134c5f0
Samuele <samuele@busnet.it>
2018-05-07 18:15:13 +0200

    advance

--- cron_script.sh
+++ cron_script.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
 
-echo "L'host:"
-echo "$MYSQL_HOST"
-echo "Il db:"
-echo "$MYSQL_DATABASE"
-echo "Lo user:"
-echo "$MYSQL_USER"
-echo "La password:"
-echo "$MYSQL_PASSWORD"
-echo "Il file:"
-echo "$MYSQL_SQL_FILENAME"
-
 _now=$(date +"%s_%m_%d_%Y")
 _file="dumpdb/$MYSQL_SQL_FILENAME_$_now.sql"
 
@@ -19,9 +8,17 @@ echo $_file
 
 mysqldump  "$MYSQL_DATABASE" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" > $_file
 
+if [ "$ZIP_FILE" = "true" ]; then
+    echo "Compress..."
+    tar -cvzf dumpdb/$MYSQL_SQL_FILENAME_$_now.tar.gz $_file
+    rm $_file
+    _file=dumpdb/$MYSQL_SQL_FILENAME_$_now.tar.gz
+
+fi
+
 
 if [ "$S3_UPLOAD" = "true" ]; then
-echo "Devo caricarlo su minio..."
+echo "S3 upload..."
 
 bucket="$S3_BUCKET"
 
